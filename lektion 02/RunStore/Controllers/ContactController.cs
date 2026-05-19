476364975d43719f29cd7a371720d67ca979e86a
mohammedalialhilo <49024733+mohammedalialhilo@users.noreply.github.com>
2026-05-19 13:42:05 +0200

    L5 13:41

--- lektion 02/RunStore/Controllers/ContactController.cs
+++ lektion 02/RunStore/Controllers/ContactController.cs
@@ -1,13 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using RunStore.Models;
 
 namespace RunStore.Controllers
 {
     public class ContactController : Controller
     {
         // GET: ContactController
+        [HttpGet()]
         public ActionResult Index()
         {
-            return View();
+            var model = new ContactModel();
+            return View(model);
+        }
+        [HttpPost()]
+        public ActionResult SendInfo(ContactModel model)
+        {
+
+            return View("Confirmation", model);
+
         }
 
     }
---
+++ lektion 02/RunStore/Models/ContactModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RunStore.Models;
+
+public class ContactModel
+{
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string Massage { get; set; } = "";
+
+}
