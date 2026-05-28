57f9080811bc9f5bfcd83be4d482eb3d00322a89
mohammedalialhilo <49024733+mohammedalialhilo@users.noreply.github.com>
2026-05-28 12:06:01 +0200

    L8 12:05

--- Lektion_07/RunStoreIntroDom/src/Data/products.js
+++ Lektion_07/RunStoreIntroDom/src/Data/products.js
@@ -1,4 +1,4 @@
-const data = [
+const products = [
   {
     description:
       "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
---
+++ Lektion_07/RunStoreIntroDom/src/pages/products/product-details.js
@@ -0,0 +1,36 @@
+const heading = document.querySelector("h1");
+const productDetails = document.querySelector("#product-details");
+
+const initApp = () => {
+  console.log(location.search.split("=")[1]);
+  const id = location.search.split("=")[1];
+
+  if (!id) return;
+
+  const product = findProduct(id);
+  if (!product) return;
+
+  displayProduct(product);
+};
+
+const findProduct = (id) => {
+  return products.find((product) => product.id === id);
+};
+
+const displayProduct = (product) => {
+  let html = " ";
+  heading.innerText = product.name;
+
+  html = /*html*/ `
+    <article>
+      <img src="../../assets${product.imageUrl}" alt="${product.name}">
+      <section>
+        <p>Beskrivning: <br> ${product.description}</p>
+      </section>
+      <p>Pris: ${product.price.toLocaleString("se-SV", { minimumFractionDigits: 2 })} Kr</p>
+    </article>
+  `;
+
+  productDetails.innerHTML = html;
+};
+initApp();
